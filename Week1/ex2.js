bc14bdfb54d2e3c00f8bbfefd4ca358de3881165
Alaa <angnm38@gmail.com>
2025-11-30 14:41:22 +0100

    Modify select queries in ex2.js

--- Week1/ex2.js
+++ Week1/ex2.js
@@ -12,10 +12,7 @@ const config = {
 const client = new Client(config);
 
 async function selectQuery(selectQuery) {
-  const query1 = selectQuery;
-  const query1Results = await client.query(query1);
-
-  return query1Results;
+  return await client.query(selectQuery);
 }
 
 function logResults(queryResults) {
@@ -26,66 +23,77 @@ function logResults(queryResults) {
 
 async function query1() {
   // 1. What are the names of countries with population greater than 8 million?
-  const query = "SELECT Name FROM country WHERE Population > 8000000";
   console.log(
     "The names of countries with population greater than 8 million: "
   );
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name FROM country WHERE Population > 8000000"
+  );
+  logResults(results);
   console.log("\n....................................................\n");
 }
 
 async function query2() {
   // 2. What are the names of countries that have "land" in their names?
-  const query = "SELECT Name FROM country WHERE Name ILIKE '%land%'";
   console.log("The names of countries that have 'land' in their names: ");
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name FROM country WHERE Name ILIKE '%land%'"
+  );
+  logResults(results);
   console.log("\n....................................................\n");
 }
 
 async function query3() {
   // 3. What are the names of the cities with population in between 500,000 and 1 million?
-  const query =
-    "SELECT Name FROM city WHERE Population between 500000 AND 1000000";
   console.log(
     "The names of the cities with population in between 500,000 and 1 million: "
   );
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name FROM city WHERE Population between 500000 AND 1000000"
+  );
+  logResults(results);
   console.log("\n....................................................\n");
 }
 
 async function query4() {
   // 4. What's the name of all the countries on the continent 'Europe'?
-  const query = "SELECT Name FROM country WHERE Continent LIKE 'Europe%'";
   console.log("The name of all the countries on the continent 'Europe': ");
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name FROM country WHERE Continent LIKE 'Europe%'"
+  );
+  logResults(results);
   console.log("\n....................................................\n");
 }
 
 async function query5() {
   // 5. List all the countries in the descending order of their surface areas.
-  const query = "SELECT Name FROM country ORDER BY SurfaceArea DESC";
   console.log(
     "List of all the countries in the descending order of their surface areas: "
   );
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name FROM country ORDER BY SurfaceArea DESC"
+  );
+  logResults(results);
   console.log("\n....................................................\n");
 }
 
 async function query6() {
   //   6. What are the names of all the cities in the Netherlands?
-  const query =
-    "SELECT Name FROM city WHERE CountryCode IN (SELECT Code FROM country WHERE name like '%Netherlands%')";
   console.log("List of all cities in the Netherlands: ");
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name FROM city WHERE CountryCode = 'NLD'"
+  );
+  logResults(results);
 
   console.log("\n....................................................\n");
 }
 
 async function query7() {
   // 7. What is the population of Rotterdam?
-  const query = "SELECT Population FROM city WHERE name like '%Rotterdam%'";
   console.log("The population of Rotterdam: ");
-  const result = await client.query(query);
+  const result = await client.query(
+    "SELECT Population FROM city WHERE name like '%Rotterdam%'"
+  );
   console.log(result.rows[0].population);
 
   console.log("\n....................................................\n");
@@ -93,28 +101,30 @@ async function query7() {
 
 async function query8() {
   //  8. What's the top 10 countries by Surface Area?
-  const query =
-    "SELECT Name, SurfaceArea FROM country ORDER BY SurfaceArea DESC LIMIT 10";
   console.log("Top 10 countries by Surface Area: ");
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name, SurfaceArea FROM country ORDER BY SurfaceArea DESC LIMIT 10"
+  );
+  logResults(results);
 
   console.log("\n....................................................\n");
 }
 
 async function query9() {
   //  9. What's the top 10 most populated cities?
-  const query =
-    "SELECT Name, Population FROM city ORDER BY Population DESC LIMIT 10";
   console.log("The 10 most populated cities: ");
-  logResults(await selectQuery(query));
+  const results = await selectQuery(
+    "SELECT Name, Population FROM city ORDER BY Population DESC LIMIT 10"
+  );
+  logResults(results);
   console.log("\n....................................................\n");
 }
 
 async function query10() {
   // 10. What is the population number of the world?
-  const query = "SELECT SUM(Population) FROM country";
+
   console.log("The population number of the world: ");
-  const result = await client.query(query);
+  const result = await client.query("SELECT SUM(Population) FROM country");
   console.log(result.rows[0].sum);
 
   console.log("\n....................................................\n");
